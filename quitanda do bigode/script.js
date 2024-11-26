cf35f55bf381af994ad8c2edca36729adfa67c5e
Tamires Rodrigues Delmondes <89863897+tamiresrodrigue@users.noreply.github.com>
2024-11-25 21:14:24 -0300

    Add files via upload

---
+++ quitanda do bigode/script.js
@@ -0,0 +1,21 @@
+const inicioPrograma = Number(prompt(`
+    ============== Bem-Vindo ==========
+    O que deseja comprar?
+    ===================================
+     Selecione uma opção
+     __________________________________
+    [1] -Banana
+    [2] -Maçã
+    [3] -Manga
+    [4] -Laranja
+    [5] -Limão`
+))
+switch (inicioPrograma) {
+    case 1:
+        alert("Banana")
+        
+        break;
+
+    default:
+        break;
+}
